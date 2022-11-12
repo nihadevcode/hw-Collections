c95fb661227af9e4b4b1464b4b20a72f1e494a69
Nihad Valiyev <nihadev@code.edu.az>
2022-11-13 01:00:51 +0400

    adding homework

---
+++ Collections/Authors.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Collections
+{
+    public class Authors
+    {
+        public string FullName { get; set; }
+        public int Age { get; set; }
+
+        //public Authors(string _fullname, int )
+        //{
+        //    FullName = _fullname;
+
+        //}
+
+        public override string ToString()
+        {
+            return $"AuthorsName: {this.FullName} - Age: {this.Age}";
+        }
+    }
+}
---
+++ Collections/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Authors> AuthorList = new List<Authors>()
+            {
+                new Authors() {FullName = "Chingiz Abdullayev", Age = 67},
+                new Authors() {FullName = "Aqata Kristi", Age = 26},
+                new Authors() {FullName = "Artur Konan Doyl", Age = 30},
+                new Authors() {FullName = "Nizami Gencevi", Age = 20},
+                new Authors() {FullName = "Fuzuli", Age = 350}
+            };
+            
+            foreach (Authors item in AuthorList)
+            {
+                if (Authors.Age > 40)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+    }
+}
